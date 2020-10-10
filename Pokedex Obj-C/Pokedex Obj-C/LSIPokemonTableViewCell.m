93688ed83d6186e5c970a02c9766ecd3fe94671f
Waseem Idelbi <idelbiw@gmail.com>
2020-10-10 00:20:29 -0400

    added all the UI elements for the detail view controller

--- Pokedex Obj-C/Pokedex Obj-C/LSIPokemonTableViewCell.m
+++ Pokedex Obj-C/Pokedex Obj-C/LSIPokemonTableViewCell.m
@@ -9,15 +9,5 @@
 
 @implementation LSIPokemonTableViewCell
 
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
-}
 
 @end
